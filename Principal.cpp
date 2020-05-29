fafb5a1b2dba590d9b53b4b0587db80c75b5ac0f
juan ruiz <juanpablo91-gmail.com>
2020-05-29 16:58:43 -0500

    Mi primer commit subida de estructura vacia de un programa en c++

---
+++ Principal.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fibonaci(int n)
+{
+//Inserccion de codigo
+return 0;
+
+
+}
+int factorial(int p)
+{
+//Inserccion de codigo
+return 0;
+
+}
+main()
+{
+//Funcion principal
+ 
+}
